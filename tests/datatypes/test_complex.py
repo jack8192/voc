cd217af97ccbdfb26ef3c86d002bbd25189602da
jack8192 <jack.carson.coleman@gmail.com>
2017-05-25 10:39:16 -0700

    Remove a blank line that contains spaces

--- tests/datatypes/test_complex.py
+++ tests/datatypes/test_complex.py
@@ -43,7 +43,6 @@ class ComplexTests(TranspileTestCase):
 class UnaryComplexOperationTests(UnaryOperationTestCase, TranspileTestCase):
 
     data_type = 'complex'
-       
 
 class BinaryComplexOperationTests(BinaryOperationTestCase, TranspileTestCase):
     data_type = 'complex'
